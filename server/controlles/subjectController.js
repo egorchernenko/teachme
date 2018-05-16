c690dc41f48031785f072317a3ef43159300e324
egorchernenko <lordo916@gmail.com>
2018-05-16 12:19:33 +0300

    json test

--- server/controlles/subjectController.js
+++ server/controlles/subjectController.js
@@ -43,7 +43,7 @@ router.get('/',authenticate,function (req,res) {
 router.get('/all', function (req,res) {
 
     Subject.find().then(function (subjects) {
-        res.status(200).json({subjects: subjects})
+        res.json({subjects: subjects})
     },function (err) {
         res.status(400).send(err);
     })
